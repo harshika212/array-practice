abc50c1997fa5475cf403eb89b7bcc1903264d1e
Harshika <harshikasingh212@gmail.com>
2026-04-04 02:54:23 +0530

    initial commit

---
+++ src/linearSearch.java
@@ -0,0 +1,19 @@
+public class linearSearch {
+    public static void main(String[] args) {
+        String name="honey";
+        char target='o';
+        System.out.println(search(name,target));
+    }
+    static boolean search(String str, char target){
+        if(str.isEmpty()){
+            return false;
+        }
+        for(int i=0;i<str.length();i++){
+            if(target == str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+

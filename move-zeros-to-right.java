48fa61468d301d1992b892d1ccd52a37d5231ff4
harshika212 <harshikasingh212@gmail.com>
2026-04-11 22:39:21 +0530

    Create move-zeros-to-right.java

---
+++ move-zeros-to-right.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class sortArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter " + n + " size");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sortedResult(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void sortedResult(int[] arr) {
+        int zeroes = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                zeroes++;
+
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (i < zeroes) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+        }
+
+    }
+}

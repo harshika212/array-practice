cad903725d986b251faf288c47191c22dc3e2998
harshika212 <harshikasingh212@gmail.com>
2026-05-01 23:54:55 +0530

    Update Floor.java

--- Floor.java
+++ Floor.java
@@ -1,2 +1,18 @@
 // Floor
   
+int floor(int[] arr, int target) {
+    int start = 0, end = arr.length - 1;
+    int ans = -1;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] <= target) {
+            ans = arr[mid];   // store value
+            start = mid + 1;  // go right
+        } else {
+            end = mid - 1;
+        }
+    }
+    return ans;
+}

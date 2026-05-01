1e679598a601231577c13f199685ae5a8adabee8
harshika212 <harshikasingh212@gmail.com>
2026-05-01 23:55:52 +0530

    Update Ceil.java

--- Ceil.java
+++ Ceil.java
@@ -1,1 +1,18 @@
 // Ceil
+
+int ceil(int[] arr, int target) {
+    int start = 0, end = arr.length - 1;
+    int ans = -1;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] >= target) {
+            ans = arr[mid];   // store value
+            end = mid - 1;    // go left
+        } else {
+            start = mid + 1;
+        }
+    }
+    return ans;
+}
